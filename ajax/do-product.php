f5449bbda0b3414b3089edb48fa9402ed08f03e1
mohamedkhaled16 <eng.mohamed16@yahoo.com>
2016-02-12 15:16:00 -0500

    full ver of CRUD operations for proudcts

--- ajax/do-product.php
+++ ajax/do-product.php
@@ -9,118 +9,29 @@ $product_id="";
 		else {$product_id=$_POST['product_id'];}
 	
 //*********Delete*********//	
-  if(!isset($_POST['product_id']) || empty($_POST['product_id']) ){echo "0";}
-		else {
-		
-			$result=$admin->delete_product($product_id);
+  if(isset($_POST['delete_p']) && !empty($_POST['delete_p']) ){$result=$admin->delete_product($product_id);
 			if ($result != false) {    
 			    echo "1";
-			 }else{echo "0";}
-		}
+			 }else{echo "0";}}
+
 		
 //*********EditAvail*********//		
-   if(!isset($_POST['product_unavail']) || empty($_POST['product_unavail']) ){echo "0";}
-		else {
-		
-			$result=$admin->change_product_status_unavail($product_id);
+   if(isset($_POST['product_unavail']) && !empty($_POST['product_unavail']) ){$result=$admin->change_product_status_unavail($product_id);
 			if ($result != false) {    
 			    echo "1";
-			 }else{echo "0";}
-		}
+			 }else{echo "0";}}
+	
 		
 //*********EditUnAvial*********//		
-  if(!isset($_POST['product_avail']) || empty($_POST['product_avail']) ){echo "0";}
-		else {
-		
-			$result=$admin->change_product_status_avail($product_id);
+  if(isset($_POST['product_avail']) && !empty($_POST['product_avail']) ){$result=$admin->change_product_status_avail($product_id);
 			if ($result != false) {    
 			    echo "1";
-			 }else{echo "0";}
-		}
+			 }else{echo "0";}}
 
 
-/*
-
-	$product_name="";
-	$product_price="";
-	$category_id="";
-	$status="unavailable";
-	$display="yes";
-	$Filename = "";
-        $error='';
-
-	if(!isset($_POST['product_name']) || empty($_POST['product_name']) ){$error.="Please Enter Proudct Name <br/>";}
-		else {$product_name=$_POST['product_name']; echo "Wrong";}
-	if(!isset($_POST['product_price']) || empty($_POST['product_price']) ){$error.="Please Enter Proudct Price <br/>";}
-		else {$product_price=$_POST['product_price']; }
-	if(!isset($_POST['category_id']) || empty($_POST['category_id']) ){$error.="Please Select Proudct Category <br/>";}
-		else {$category_id=$_POST['category_id']; }
-		
-	$accepted_image_types = array("image/gif", "image/jpg", "image/jpeg", "image/pjpeg", "image/png", "image/x-png");
-	if(is_uploaded_file($_FILES['proudct_image']['tmp_name']) && is_array($_FILES)) {
-       if(!empty($_FILES['proudct_image']['tmp_name'])){
-		if ($_FILES['proudct_image']['error'] > 0)
-		{
-			switch ($_FILES['proudct_image'][‘error’])
-			{
-			case 1: $error.= 'File exceeded upload_max_filesize';
-			break;
-			case 2: $error.= 'File exceeded max_file_size';
-			break;
-			case 3: $error.= 'File only partially uploaded';
-			break;
-			case 4: $error.= 'No file uploaded';
-			break;
-			case 6: $error.= 'Cannot upload file: No temp directory specified';
-			break;
-			case 7: $error.= 'Upload failed: Cannot write to disk';
-			break;
-			}
-			//	echo $_FILES['proudct_image']['type'];
-			    }
-		elseif (!in_array($_FILES['proudct_image']['type'], $accepted_image_types ))
-		{
-			$error.= 'Problem: file is not image';
-			#exit;
-		}
-          }else {$error.="Please Add Image...";}}
-          else {$error.="Please Add Image...";}
-	
-
-
-        if(!empty($error)){
-        echo '<div class="alert alert-danger"><strong>Error!</strong>';
-        echo $error;
-        echo '</div>';
-        }else{
-          if (is_uploaded_file($_FILES['proudct_image']['tmp_name']))
-          {
-            $nameimg = time().$_FILES['proudct_image']['name'];
-            $Filename = 'uploads/'.$nameimg;
-            if (!move_uploaded_file($_FILES['proudct_image']['tmp_name'], $Filename))
-            {
-            echo '<div class="alert alert-danger"><strong>Error!</strong>';
-            echo 'Problem: Could not move file to destination directory';
-            echo '</div>';
-            exit;
-            }
-          }
-           $data =array("product_name"=>"{$product_name}","product_price"=>"{$product_price}","category_id"=>"{$category_id}","status"=>"{$status}","display"=>"{$display}","image"=>"{$Filename}");
-         var_dump($data);
-          $result=$admin->insert_product($data);  
-          if ($result != false) {    
-            echo '<div class="alert alert-success"><strong>Success!</strong> User had benn added...</div>';
-         }else{
-		 echo '<div class="alert alert-danger"><strong>Error!</strong>';
-		 echo"Can't add this user...";
-		 echo '</div>';
-		  array_map('unlink', glob($Filename)); 
-         }
-   }
-
-
-
-
-
-*/
 ?>
+
+
+
+
+
--- all-products.php
+++ all-products.php
@@ -11,28 +11,38 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <script>
 function changeProudctStatusUnAvail(PID){
-$.post("ajax/do-delete-product.php",
+$.post("ajax/do-product.php",
     {
         product_id: PID,
         product_unavail: "product_unavail"
     },
     function(data, status){
-    if(  status == "success" && data =="1"){
-    $('#row'+PID).remove();
+    if(  status == "success" && data == 1){    
+    $('#status'+PID).removeClass("text-success");
+	    $('#status'+PID).addClass("text-danger");
+	    $('#statusspan'+PID).html("UnAvialble");
+	$('#status'+PID).attr("onclick","changeProudctStatusAvail("+PID+")");
+	$('#status'+PID).html('<i class="fa fa-toggle-off fa-3x"></i>');
     }
 
     });
 }
 
 function changeProudctStatusAvail(PID){
-$.post("ajax/do-delete-product.php",
+$.post("ajax/do-product.php",
     {
         product_id: PID,
         product_avail: "product_avail"
     },
     function(data, status){
-    if(  status == "success" && data =="1"){
-    $('#row'+PID).remove();
+    if(  status == "success" && data == 1){
+	   $('#status'+PID).addClass("text-success");
+    $('#status'+PID).removeClass("text-danger"); 
+    $('#statusspan'+PID).html("Avialble");
+    $('#status'+PID).attr("onclick","changeProudctStatusUnAvail("+PID+")");
+    $('#status'+PID).html('<i class="fa fa-toggle-on fa-3x"></i>');
+    
+    
     }
 
     });
@@ -40,12 +50,15 @@ $.post("ajax/do-delete-product.php",
 
 function deleteProudct(PID){
 
-$.post("ajax/do-delete-product.php",
+$.post("ajax/do-product.php",
     {
-        product_id: PID
+        product_id: PID,
+        delete_p: "delete_p"
     },
     function(data, status){
-    if(  status == "success" && data =="1"){
+    //alert(data+"status"+status);
+    if(status == "success"){
+    //alert(PID);
     $('#row'+PID).remove();
     }
 
@@ -90,9 +103,9 @@ alert(PID);
 				echo "<td><img style='width:200px' class='img-rounded img-responsive'  src='$image'></td>";
 				echo '<td>';
 				if($status == "unavailable"){
-				echo 'Unavailble<a href="javascript:void(0);" class="text-danger" onclick="changeProudctStatusAvail('.$product_id.')" id="status'.$product_id.'"><i class="fa fa-toggle-on fa-3x"></i></a>';}
+				echo '<span id="statusspan'.$product_id.'" >UnAvailable</span><a href="javascript:void(0);" class="text-danger" onclick="changeProudctStatusAvail('.$product_id.')" id="status'.$product_id.'"><i class="fa fa-toggle-off fa-3x"></i></a>';}
 				else{
-				echo 'Available<a href="javascript:void(0);" class="text-success" onclick="changeProudctStatusUnAvail('.$product_id.')" id="status'.$product_id.'"><i class="fa fa-toggle-on fa-3x"></i></a>';
+				echo '<span id="statusspan'.$product_id.'" >Available</span><a href="javascript:void(0);" class="text-success" onclick="changeProudctStatusUnAvail('.$product_id.')" id="status'.$product_id.'"><i class="fa fa-toggle-on fa-3x"></i></a>';
 				}
 				echo '<a href="javascript:void(0);" class="text-primary" onclick="editProudct('.$product_id.')" id="edit'.$product_id.'"><i class="fa fa-pencil-square-o fa-3x"></i></a>';
 				echo '<a href="javascript:void(0);" class="text-danger" onclick="deleteProudct('.$product_id.')" id="delete'.$product_id.'"><i class="fa fa-trash-o fa-3x"></i></a>';
