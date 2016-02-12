6cebf8c1f888435d03af711cc03ab1e10185639e
mohamedkhaled16 <eng.mohamed16@yahoo.com>
2016-02-12 09:31:57 -0500

    removed echoes & exit for testing

--- ajax/get-categories.php
+++ ajax/get-categories.php
@@ -1,5 +1,4 @@
 <?php 
-echo __DIR__;
    error_reporting(E_ALL);
    ini_set('display_errors', 1);
  require_once __DIR__."/../include/classes_header.php" ;
--- include/classes_header.php
+++ include/classes_header.php
@@ -2,7 +2,6 @@
    error_reporting(E_ALL);
    ini_set('display_errors', 1);
    session_start();
-   echo "dsakd";
    include __DIR__."/dblib.php";
    include __DIR__."/admin_class.php";
    include __DIR__."/shared_classes.php";
