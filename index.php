643f6f7b529efd6ec1b12e311d86a2002a2273fa
homa <engreham.ali2011@gmail.com>
2016-02-13 22:26:56 +0200

    admin home

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ include("include/header.php");
 
 if($_SESSION['usertype']=='user'){
 	header("location:order-user.php");
+}elseif($_SESSION['usertype']=='admin'){
+	header("location:admin-home.php");	
 }
  include("include/footer.php");
  ?>
