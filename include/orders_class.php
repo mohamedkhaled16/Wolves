0ff1afc377af0290aa11f0bbc60d00eecfa659f1
homa <engreham.ali2011@gmail.com>
2016-02-13 18:55:58 +0200

    last order

--- include/orders_class.php
+++ include/orders_class.php
@@ -8,15 +8,15 @@ class orders extends ProudctDB{
       return $query;
   	}
   function getLastOrder(){
-     $tables="orders,orders_details,products";
+     $tables="orders_details,products";
      $data=[];
-     $condition="orders_details.order_id=orders.order_id 
+     $condition="orders_details.order_id=(select order_id  from orders order by order_id  desc limit 1) 
                  and orders_details.user_id='{$_SESSION['user_id']}'
                  and products.product_id= orders_details.product_id
-                 order by orders_details.order_id desc
-                 limit 1";
+                 order by orders_details.order_id desc";
      $query=$GLOBALS['db']->select($tables,$data,$condition);
      return $query;
+    
     // var_dump($query);
   }
 }
